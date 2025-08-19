1f8774f373d3ff62efd707861fc9e7651f950280
chuchart saetiew <samchat2561@gmail.com>
2025-08-19 12:24:50 +0700

    update

--- app/components/app/header.vue
+++ app/components/app/header.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="font-[sans-serif] bg-gray-200 min-h-screen">
+  <div class="font-[sans-serif] bg-gray-200 max-h-screen">
     <!-- Start Header -->
     <div class="text-gray-600 bg-[#AC1CE3]">
-      <nav class="container px-6 py-[0.8rem] mx-auto md:flex md:justify-between md:items-center shadow-xl pl-5 md:px-30 z-0 cursor-pointer">
+      <nav class="container px-6 py-[0.4rem] mx-auto md:flex md:justify-between md:items-center shadow-xl pl-5 md:px-30 z-0 cursor-pointer">
         <NuxtLink to="/" class="flex justify-center items-center text-white text-md md:text-2xl font-bold hover:text-blue-200">
           <span class="text-center ml-2">Dev</span>
         </NuxtLink>
