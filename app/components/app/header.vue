00ee9eb84d14c44980bf5393fb4aaff8d9c2a596
chuchart saetiew <samchat2561@gmail.com>
2025-08-19 12:05:07 +0700

    update

---
+++ app/components/app/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="font-[sans-serif] bg-gray-200 min-h-screen">
+    <!-- Start Header -->
+    <div class="text-gray-600 bg-[#AC1CE3]">
+      <nav class="container px-6 py-[0.8rem] mx-auto md:flex md:justify-between md:items-center shadow-xl pl-5 md:px-30 z-0 cursor-pointer">
+        <h1>Header</h1>
+      </nav>
+    </div>
+    <!-- End Header -->
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AppHeader />
+    <slot />
+    <AppFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
