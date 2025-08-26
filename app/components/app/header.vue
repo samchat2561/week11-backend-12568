17ebdbd20068d634c0d715c194f9a2be413c5cfd
samchat2561 saetieaw <samchat2561@gmail.com>
2025-08-26 09:59:20 +0700

    update

--- app/components/app/header.vue
+++ app/components/app/header.vue
@@ -11,9 +11,11 @@
         </NuxtLink>
 
         <!-- Start mobile menu button -->
-        <div>
-          <button>
-            
+        <div class="md:hidden">
+          <button type="button" class="text-white hover:text-gray-400 focus:text-gray-300">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
           </button>
         </div>
         <!-- End mobile menu button -->
