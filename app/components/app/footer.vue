b57c00260322942903558beee62cf9a4d59183f1
samchat2561 saetieaw <samchat2561@gmail.com>
2025-09-02 10:38:24 +0700

    update

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -4,6 +4,10 @@
       <div class="bg-[#ffffff] rounded-b-lg gap-3 rounded-md shadow-2xl flex flex-wrap items-center justify-between sm:px-8 max-sm:px-4 py-5 absolute top-[-66px] w-full"> 
         <h1>Footer</h1>
       </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 pt-15 px-4 rounded-b-md bg-[#802918]">
+
+      </div>
       
     </div>
   </footer>
