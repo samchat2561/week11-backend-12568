917f8100495a5041a4bd9b149f350ee551eddebb
samchat2561 saetieaw <samchat2561@gmail.com>
2025-09-02 11:32:34 +0700

    update

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -83,7 +83,7 @@
           <hr class="w-54 bg-gray-100 border-spacing-x-0.5 text-white rounded-sm dark:bg-gray-700" />
           <ul class="flex flex-wrap gap-4 mt-2">
               <li>
-                <a href='javascript:void(0)'>
+                <a href='https://www.facebook.com/profile.php?id=100057385753216#' target="_blank">
                   <svg xmlns="http://www.w3.org/2000/svg" class="fill-blue-600 w-8 h-8" viewBox="0 0 49.652 49.652">
                     <path d="M24.826 0C11.137 0 0 11.137 0 24.826c0 13.688 11.137 24.826 24.826 24.826 13.688 0 24.826-11.138 24.826-24.826C49.652 11.137 38.516 0 24.826 0zM31 25.7h-4.039v14.396h-5.985V25.7h-2.845v-5.088h2.845v-3.291c0-2.357 1.12-6.04 6.04-6.04l4.435.017v4.939h-3.219c-.524 0-1.269.262-1.269 1.386v2.99h4.56z" data-original="#000000" />
                   </svg>
