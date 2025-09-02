98dc2aa449c3f2e4701f0c180e49e22a7f44d95d
samchat2561 saetieaw <samchat2561@gmail.com>
2025-09-02 10:57:30 +0700

    update

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -1,14 +1,20 @@
 <template>
-  <footer class="bg-[#802918] py-6 px-8 mt-4 tracking-wide">
+  <footer class="font-[Sarabun] bg-[#802918] py-6 px-8 mt-4 tracking-wide">
     <div class="relative max-w-screen-xl mx-auto">
       <div class="bg-[#ffffff] rounded-b-lg gap-3 rounded-md shadow-2xl flex flex-wrap items-center justify-between sm:px-8 max-sm:px-4 py-5 absolute top-[-66px] w-full"> 
-        <h1>Footer</h1>
+        <h6 class="text-[#7c150d] sm:text-xl max-sm:text-base font-bold">วิทยาลัยเทคนิคตราด</h6>
+        <button type="button" class="px-4 py-2 text-[15px] font-semibold text-white bg-[#802918] rounded-full gap-2 cursor-pointer">
+          สํานักงานคณะกรรมการการอาชีวศึกษา
+        </button>
       </div>
 
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 pt-15 px-4 rounded-b-md bg-[#802918]">
-
+        bb
       </div>
-      
+    </div>
+
+    <div class="bg-[#802918] pt-5 sm:px-30 w-full">
+      xx
     </div>
   </footer>
 </template>
